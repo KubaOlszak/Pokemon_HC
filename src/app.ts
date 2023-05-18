edbb1335d4ce00a2c60ec80d0e1db2997dc11855
Kuba OLSZAK <analogband@gmail.com>
2023-05-18 22:50:23 +0200

    Commit to backup 2

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,37 @@
-const test = 'test';
-console.log(test);
+/* const room: {
+  quantity: number | null;
+  pokemonName: string | null;
+} = {
+  quantity: 0,
+  pokemonName: '',
+}
+ */
+// const health: number[];
+
+const waitingRoom = {
+  quantity: 0,
+  pokemonName: [],
+
+}
+
+const healthCenter = {
+  quantity: 0,
+  pokemonName: [],
+  //healingPokemon: function ;
+}
+
+const enterBtnElt = document.querySelector('.pokemon_card .btn')!;
+enterBtnElt.addEventListener("click", enterInCenter);
+
+function enterInCenter() {
+  const pokemonElt = document.getElementById('bulbi')!;
+  pokemonElt.classList.add('hidePokemon');
+
+  // console.log(pokemonElt);
+  // const waitingroomElt = document.querySelector('#waiting-room')!;
+  // waitingroomElt.appendChild(pokemonElt);
+}
+
+function enter() {
+
+}
