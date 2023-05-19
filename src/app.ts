67d80ec4b5e914fa4b5d75dea1ed6c20cbba2e95
Kuba OLSZAK <analogband@gmail.com>
2023-05-19 07:48:17 +0200

    Commit de backup 4

--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,46 @@
-/* const room: {
-  quantity: number | null;
-  pokemonName: string | null;
-} = {
-  quantity: 0,
-  pokemonName: '',
-}
- */
-// const health: number[];
+
+const maxCapacity = 6;
+let currentCapacity = 0;
+const isInside: (boolean) = false;
 
 const waitingRoom = {
-  quantity: 0,
-  pokemonName: [],
-
+  capacity: 0,
 }
 
 const healthCenter = {
-  quantity: 0,
-  pokemonName: [],
+  capacity: 0,
   //healingPokemon: function ;
 }
 
-const enterBtnElt = document.querySelector('.pokemonOut1 .btn')!;
+const bulbi = {
+  name: "Bulbizarre",
+  health: 10,
+  isInside: false,
+}
+
+const enterBtnElt = document.querySelector(".pokemonOut1 .btn")!;
 enterBtnElt.addEventListener("click", enterInCenter);
 
 function enterInCenter() {
-  const pokemonElt = document.querySelector('.pokemonOut1')!;
-  pokemonElt.classList.add('hidePokemon');
+  const pokemonElt = document.querySelector(".pokemonOut1")!;
+  const pokemonEltClone = pokemonElt.cloneNode(true);
+  pokemonElt.classList.add("hidePokemon");
 
-  // console.log(pokemonElt);
+  const waitingPlace = document.querySelector(".pokemonIn1");
+  waitingPlace?.appendChild(pokemonEltClone);
+  const imageElt = document.querySelector("#bulbi")!;
+  imageElt.removeAttribute("src");
+  imageElt.setAttribute("src", "img/1_inside.png");
+
+  waitingRoom.capacity++;
+
+  const counter = document.querySelector("#counter");
+  // counter.
+  console.log(waitingRoom.capacity);
   // const waitingroomElt = document.querySelector('#waiting-room')!;
   // waitingroomElt.appendChild(pokemonElt);
 }
 
-function enter() {
+function incrementCapacity() {
 
 }
