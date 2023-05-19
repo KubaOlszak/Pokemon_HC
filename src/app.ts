3e3f570e423d979ea9763adaa188ed65fc60a8df
Philippe Rimbaut <phil.rimbaut@gmail.com>
2023-05-19 14:21:14 +0200

    incrementation "counter" + "nom pokemon present"

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,21 @@
-
 const maxCapacity = 6;
 let currentCapacity = 0;
-const isInside: (boolean) = false;
+const isInside: boolean = false;
 
 const waitingRoom = {
   capacity: 0,
-}
+};
 
 const healthCenter = {
   capacity: 0,
   //healingPokemon: function ;
-}
+};
 
 const bulbi = {
   name: "Bulbizarre",
   health: 10,
   isInside: false,
-}
+};
 
 const enterBtnElt = document.querySelector(".pokemonOut1 .btn")!;
 enterBtnElt.addEventListener("click", enterInCenter);
@@ -30,11 +29,24 @@ function enterInCenter() {
   waitingPlace?.appendChild(pokemonEltClone);
   const imageElt = document.querySelector("#bulbi")!;
   imageElt.removeAttribute("src");
-  imageElt.setAttribute("src", "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png");
+  imageElt.setAttribute(
+    "src",
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png"
+  );
 
   waitingRoom.capacity++;
 
   const counter = document.querySelector("#counter");
+  if (counter) {
+    counter.textContent = waitingRoom.capacity.toString();
+  }
+
+  const pokemonList = document.querySelector(".nomPokemonsPresents ul");
+  if (pokemonList) {
+    let pokemonElt = document.createElement("li");
+    pokemonElt.textContent = bulbi.name;
+    pokemonList.appendChild(pokemonElt);
+  }
 
   console.log(waitingRoom.capacity);
 }
