2eaa18107c176f5027408810df12cccf5d281cb3
Kuba OLSZAK <analogband@gmail.com>
2023-05-21 11:37:33 +0200

    Check if not < 0 and not over set capacity

--- src/app.ts
+++ src/app.ts
@@ -18,23 +18,49 @@ const bulbi = {
   isInside: false,
 }
 
+function isPlacesNumberNegative() {     // vérifie si nombre de places pas < 0
+  if (waitingRoom.capacity < 0) {
+    alert("Nombre de places négatif !");
+    return true;
+  }
+}
+
+function isPlacesNumberOverCapacity() {     // vérifie si nombre de places pas < 0
+  if (waitingRoom.capacity >= 6) {
+    alert("Plus de places disponibles !");
+    return true;
+  }
+}
+
+window.onload = (event) => {            // affiche le nombre de places dispo au chargement de la page
+  // isPlacesNumberNegative();
+  const counter: (HTMLElement | null) = document.querySelector("#counter")!;
+  counter.textContent = waitingRoom.capacity.toString();
+}
+
 const enterBtnElt = document.querySelector(".pokemonOut1 .btn")!;
 enterBtnElt.addEventListener("click", enterInCenter);
 
 function enterInCenter() {
-  const pokemonElt = document.querySelector(".pokemonOut1")!;
-  const pokemonEltClone = pokemonElt.cloneNode(true);
-  pokemonElt.classList.add("hidePokemon");
+  if (isPlacesNumberNegative() || isPlacesNumberOverCapacity()) return;
 
-  const waitingPlace = document.querySelector(".pokemonIn1");
-  waitingPlace?.appendChild(pokemonEltClone);
+  const pokemonOutElt = document.querySelector(".pokemonOut1")!;
+  const pokemonEltClone = pokemonOutElt.cloneNode(true);
+  pokemonOutElt.classList.add("hidePokemon");
+
+  const waitingPlace = document.querySelector(".pokemonIn1")!;
+  waitingPlace.appendChild(pokemonEltClone);
   const imageElt = document.querySelector("#bulbi")!;
   imageElt.removeAttribute("src");
   imageElt.setAttribute("src", "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png");
 
   waitingRoom.capacity++;
 
-  const counter = document.querySelector("#counter");
+  const counter: (HTMLElement | null) = document.querySelector("#counter")!;
+  counter.textContent = waitingRoom.capacity.toString();
 
-  console.log(waitingRoom.capacity);
+  const pokemonList = document.querySelector(".nomPokemonsPresents ul")!;
+  const pokemonInElt = document.createElement("li");
+  pokemonInElt.textContent = bulbi.name;
+  pokemonList.appendChild(pokemonInElt);
 }
